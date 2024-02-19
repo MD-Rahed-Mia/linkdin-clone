71a3b7e359c3e3700bd944de465bcb0e1404b843
Md Rahed Mia <mdrahed708@gmail.com>
2024-02-19 21:33:07 +0600

    fixed route system

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../configAuth";
-import { useNavigate } from "react-router-dom";
-import Loader from "../components/common/Loader";
 import PostStatus from "./PostStatus";
 import { ToastContainer } from "react-toastify";
 
